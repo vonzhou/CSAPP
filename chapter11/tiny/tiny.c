0a38d5ffb542a38b900731adae75703092cd75ac
vonzhou <vonzhou@163.com>
2016-05-07 19:45:34 -0700

    tiny web server

--- chapter11/tiny/tiny.c
+++ chapter11/tiny/tiny.c
@@ -18,27 +18,24 @@ int main(int argc, char **argv)
     int listenfd, connfd, port, clientlen;
     struct sockaddr_in clientaddr;
 
-    /* Check command line args */
     if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(1);
+	   fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	   exit(1);
     }
     port = atoi(argv[1]);
 
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
-	doit(connfd);                                             //line:netp:tiny:doit
-	Close(connfd);                                            //line:netp:tiny:close
+	   clientlen = sizeof(clientaddr);
+	   connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); 
+	   doit(connfd);                                             
+	   Close(connfd);                                           
     }
 }
-/* $end tinymain */
 
 /*
  * doit - handle one HTTP request/response transaction
  */
-/* $begin doit */
 void doit(int fd) 
 {
     int is_static;
@@ -83,7 +80,6 @@ void doit(int fd)
 	serve_dynamic(fd, filename, cgiargs);            //line:netp:doit:servedynamic
     }
 }
-/* $end doit */
 
 /*
  * read_requesthdrs - read and parse HTTP request headers
